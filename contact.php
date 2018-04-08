6c568a26be057c30e8ab907aae62b4b5fb89a2d6
Sean <seanysean700@gmail.com>
2018-04-08 18:49:41 -0300

    Simple contact page

---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include "include/functions.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact • LearnChess</title>
+        <?php include "include/head.php" ?>
+    </head>
+    <body>
+        <div class="top">
+        <?php include "include/topbar.php" ?>
+        </div>
+        <div class="page">
+            <div class="main center">
+                <div class="block transparent">
+                    <h1 class="block-title"><i class="fa fa-envelope"></i> Contact LearnChess</h1>
+                    <p><a href="https://lichess.org/inbox/new?user=seanysean" target="_blank">Message Sean on lichess <i class="fa fa-external-link"></i></a></p>
+                </div>
+            </div>
+        </div>
+        <footer>
+        <?php include "include/footer.php" ?>
+        </footer>
+        <script src="js/global.js"></script>
+    </body>
+</html>
