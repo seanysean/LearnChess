84c110b5a69ff6605d91e0e4a60de2fa5b607dd7
Sean <seanysean700@gmail.com>
2018-04-08 08:58:17 -0300

    Update changes

--- js/changes.js
+++ js/changes.js
@@ -1,14 +1,34 @@
 const changeContainer = document.getElementById('changes-container');
+
 const changes = [
     {
+        date: 'Apr. 6 2018',
+        message: 'Some random text',
+        changesList: [
+            
+        ]
+    },
+    {
+        date: 'Apr. 5 2018',
+        message: 'Some nice profile additions',
+        changesList: [
+            'You can now link to your chess.com account',
+            'If you have your chess.com username specified in your profile settings, your chess.com rating will show up on your profile',
+            'You can also show your lichess rating by entering your lichess username on the settings page',
+            'Now, you are only shown your read notifications when you click on the bell icon',
+            'Notifications page now includes a button to mark all notifications as read'
+        ]
+    },
+    {
         date: 'Apr. 4 2018',
-        message: `Minor updates.`,
+        message: 'Minor updates.',
         changesList: [
             'The changes page added (this one)',
             'Profile page now shows if account is closed'
         ]
     }
-]
+];
+
 changes.forEach(c=>{
     const change = document.createElement('div');
     let linkableDate = c.date.replace(/\s/g,'-').split('.').join('').toLowerCase();
