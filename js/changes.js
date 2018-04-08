2ff0b212bcf4ca9371a5188618499523e942c9b1
Sean <seanysean700@gmail.com>
2018-04-08 18:54:55 -0300

    Update changes

--- js/changes.js
+++ js/changes.js
@@ -2,10 +2,24 @@ const changeContainer = document.getElementById('changes-container');
 
 const changes = [
     {
-        date: 'Apr. 6 2018',
-        message: 'Some random text',
+        date: 'Apr. 8 2018',
+        message: '',
         changesList: [
-            
+            'You can now <a href="/settings/password">change your password</a>',
+            'Updated the close account page',
+            'Updated the footer to be more then just a link to this page',
+            'Added simple contact page which is really just a link to contact me on lichess',
+            '<a href="404" target="_blank">Custom 404 error pages</a>'
+        ]
+    },
+    {
+        date: 'Apr. 6 2018',
+        message: '<b>NOTE</b>: These changes were not added until April 8.',
+        changesList: [
+            'Updated the changes page look (this page)',
+            'Your contributed puzzles are now on your profile',
+            'Some small notifications updates',
+            'Other small tweaks'
         ]
     },
     {
@@ -15,7 +29,7 @@ const changes = [
             'You can now link to your chess.com account',
             'If you have your chess.com username specified in your profile settings, your chess.com rating will show up on your profile',
             'You can also show your lichess rating by entering your lichess username on the settings page',
-            'Now, you are only shown your read notifications when you click on the bell icon',
+            'Now you are only shown your read notifications when you click on the bell icon',
             'Notifications page now includes a button to mark all notifications as read'
         ]
     },
