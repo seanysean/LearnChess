d27abf30d56c08b94699561416daaae2df5a7427
Sean <seanysean700@gmail.com>
2018-04-08 18:50:12 -0300

    Much better footer

--- include/footer.php
+++ include/footer.php
@@ -1,1 +1,6 @@
-<a href="/changes">View recent changes</a>
+<a class="site-name" href="/home">LearnChess.tk</a>
+<div class="right">
+    <a href="/changes">Recent Updates</a> |
+    <a href="/contact">Contact</a> •
+    <a href="/puzzles">Puzzles</a>
+</div>
