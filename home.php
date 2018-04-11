5ba32b10ea47daca267bdf6715b820dac35006ca
Sean <seanysean700@gmail.com>
2018-04-10 21:18:04 -0300

    Much better home page
    Now more then just a floating bar...

--- home.php
+++ home.php
@@ -8,6 +8,7 @@ if ($l) {
         <title>LearnChess</title>
         <?php include_once "./include/head.php" ?>
         <link href="css/chessground.css" type="text/css" rel="stylesheet">
+        <link href="css/home.css" type="text/css" rel="stylesheet">
         <style>
         #puzzleOfTheDay {
             width: 200px;
@@ -25,23 +26,36 @@ if ($l) {
         </div>
         <div class="page">
             <div class="main">
-                <div class="block"></div>
+                <div class="block">
+                    <h1 class="block-title">What's new?</h1>
+                    <div id="changes-cont"></div>
+                    <a href="changes" class="changes-link"><i class="fa fa-wrench"></i> View all changes</a>
+                </div>
             </div>
             <div class="right-area">
                 <!--<div class="block">
                     <h1 class="block-title center">Puzzle of the day</h1>
                     <a href="puzzles/view/3"><div id="puzzleOfTheDay"></div></a>
                 </div>-->
+                <div class="block">
+                    <h1 class="block-title center"><?php echo $_SESSION['username'] ?></h1>
+                </div>
+                <div class="block do-stuff no-padding">
+                    <a href="/puzzles/new"><i class="fa fa-puzzle-piece"></i> Submit a puzzle</a>
+                    <a href="/settings/profile"><i class="fa fa-edit"></i> Edit profile</a>
+                </div>
             </div>
         </div>
         <footer>
             <?php include_once "./include/footer.php" ?>
         </footer>
         <script src="js/global.js"></script>
+        <script src="js/changeslist.js"></script>
+        <script src="js/home.js"></script>
         <script src="js/chessground.min.js"></script>
         <script src="js/loadposition.js"></script>
         <script>
-        loadPosition(document.getElementById('puzzleOfTheDay'),'rnbqkbnr/pppp2pp/5p2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R');
+        //loadPosition(document.getElementById('puzzleOfTheDay'),'rnbqkbnr/pppp2pp/5p2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R');
         </script>
     </body>
 </html>
---
+++ js/changeslist.js
@@ -0,0 +1,51 @@
+const changes = [
+    {
+        date: 'Apr. 9 2018',
+        message: 'Most of the changes are not visible',
+        changesList: [
+            'Improved puzzles so that now you have to be logged in to give a puzzle a trophy and you also can\'t be the puzzle author',
+            'Puzzle moves are now checked server-side',
+            'Made the home page actually somewhat interesting'
+        ]
+    },
+    {
+        date: 'Apr. 8 2018',
+        message: '',
+        changesList: [
+            'You can now <a href="/settings/password">change your password</a>',
+            'Updated the close account page',
+            'Updated the footer to be more then just a link to this page',
+            'Added simple contact page which is really just a link to contact me on lichess',
+            '<a href="404" target="_blank">Custom 404 error pages</a>'
+        ]
+    },
+    {
+        date: 'Apr. 6 2018',
+        message: '<b>NOTE</b>: These changes were not added until April 8.',
+        changesList: [
+            'Updated the changes page look (this page)',
+            'Your contributed puzzles are now on your profile',
+            'Some small notifications updates',
+            'Other small tweaks'
+        ]
+    },
+    {
+        date: 'Apr. 5 2018',
+        message: 'Some nice profile additions',
+        changesList: [
+            'You can now link to your chess.com account',
+            'If you have your chess.com username specified in your profile settings, your chess.com rating will show up on your profile',
+            'You can also show your lichess rating by entering your lichess username on the settings page',
+            'Now you are only shown your read notifications when you click on the bell icon',
+            'Notifications page now includes a button to mark all notifications as read'
+        ]
+    },
+    {
+        date: 'Apr. 4 2018',
+        message: 'Minor updates.',
+        changesList: [
+            'The changes page added (this one)',
+            'Profile page now shows if account is closed'
+        ]
+    }
+];
