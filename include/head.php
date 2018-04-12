cd7f39d2a69e991e371a698df86722012448753d
Sean <seanysean700@gmail.com>
2018-04-12 16:16:00 -0300

    Favicon

--- include/head.php
+++ include/head.php
@@ -3,3 +3,4 @@
 <link href="/css/main.css" type="text/css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css?family=Roboto:100,400" rel="stylesheet">
+<link href="/learnchess2.png" type="icon/png" rel="icon">
