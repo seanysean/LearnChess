ac95837f7b8d6223802d986854fbf3ca6f224eb2
Sean <seanysean700@gmail.com>
2018-07-21 18:15:58 -0300

    Fix auto-offline not working
    Now that the auto-offline runs once a day, I need to check for that instead of just the hours.

--- onceaday.php
+++ onceaday.php
@@ -11,7 +11,7 @@ if (mysqli_num_rows($result) > 0) {
         $d1 = date_create($d);
         $d2 = date_create($row['last_active']);
         $diff = date_diff($d1,$d2);
-        $time = ($diff->format('%H') * 60 * 60) + ($diff->format('%i') * 60) + $diff->format('%s'); 
+        $time = ($diff->format('%d') * 24 * 60 * 60) + ($diff->format('%H') * 60 * 60) + ($diff->format('%i') * 60) + $diff->format('%s'); 
         if ($time > 1500) {
             $id = $row['id'];
             $sql = "UPDATE `users` SET online='0' WHERE id=$id";
