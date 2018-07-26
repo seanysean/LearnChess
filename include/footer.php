f66f421a7f94e706276ae33d4430dee10ad8295a
Sean <seanysean700@gmail.com>
2018-07-26 08:29:53 -0300

    Update footer with new links

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,7 @@
 <a class="site-name" href="/home">LearnChess.tk</a>
 <div class="right">
-    <a href="/changes">Recent Updates</a> |
     <a href="/contact">Contact</a> •
-    <a href="/puzzles">Puzzles</a>
+    <a href="/puzzles">Puzzles</a> •
+    <a href="/coordinates">Coordinates</a> •
+    <a href="/computer">Play computer</a>
 </div>
