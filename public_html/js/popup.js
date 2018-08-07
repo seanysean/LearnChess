964c627486362b1e8a68564aeef26e8abece2c2f
Sean <seanysean700@gmail.com>
2018-08-07 09:57:07 -0300

    Popup things

---
+++ public_html/js/popup.js
@@ -0,0 +1,42 @@
+const overlay = document.createElement('div');
+overlay.classList = 'overlay';
+document.body.appendChild(overlay);
+class Popup {
+    constructor(type,info,ev) {
+        if (type === 'confirm') {
+            this.el = document.createElement('div');
+            this.el.classList = 'popup';
+            this.el.innerHTML = `<p class="popup-title">${info.title}</p>`;
+            if (info.text) {
+                this.el.innerHTML += `<p class="popup-text">${info.text}</p>`;
+            }
+            const yes = document.createElement('button'),
+                  no = document.createElement('button'),
+                  close = document.createElement('span');
+            yes.classList = 'yes-button';
+            no.classList = 'no-button';
+            close.classList = 'close fa fa-times';
+            yes.innerHTML = info.yes;
+            no.innerHTML = info.no;
+            yes.addEventListener('click',ev.yes);
+            no.addEventListener('click',ev.no);
+            close.addEventListener('click',ev.no);
+            this.el.appendChild(yes);
+            this.el.appendChild(no);
+            this.el.appendChild(close);
+            overlay.appendChild(this.el);
+        }
+    }
+    open() {
+        overlay.style.display = 'block';
+        setTimeout(()=>{
+        overlay.style.opacity = 1;
+        },1);
+    }
+    close() {
+        overlay.style.opacity = 0;
+        setTimeout(()=>{
+        overlay.style.display = 'none';
+        },300);
+    }
+}
