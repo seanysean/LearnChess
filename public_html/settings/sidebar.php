7f82614a590197bc1442df6f9ff321136a9f1853
Sean <seanysean700@gmail.com>
2018-08-08 15:52:11 -0300

    Remove duplicate css on sidebars
    On settings pages and admin pages

--- public_html/settings/sidebar.php
+++ public_html/settings/sidebar.php
@@ -1,7 +1,7 @@
 <?php 
 function sidebar($n) {
     if ($n === 1) {
-        echo "<div class=\"block links-section\">
+        echo "<div class=\"block sidebar\">
         <a href=\"/settings\" class=\"current\"><i class=\"fa fa-cog\"></i> Settings</a>
         <a href=\"profile\"><i class=\"fa fa-edit\"></i> Profile</a>
         <a href=\"password\"><i class=\"fa fa-key\"></i> Change password</a>
@@ -9,21 +9,21 @@ function sidebar($n) {
         </div>";
     }
     if ($n === 2) {
-        echo "<div class=\"block links-section\">
+        echo "<div class=\"block sidebar\">
         <a href=\"/settings\"><i class=\"fa fa-cog\"></i> Settings</a>
         <a href=\"profile\" class=\"current\"><i class=\"fa fa-edit\"></i> Profile</a>
         <a href=\"password\"><i class=\"fa fa-key\"></i> Change password</a>
         <a href=\"close\"><i class=\"fa fa-close\"></i> Close Account</a>
         </div>";
     } else if ($n === 3) {
-        echo "<div class=\"block links-section\">
+        echo "<div class=\"block sidebar\">
         <a href=\"/settings\"><i class=\"fa fa-cog\"></i> Settings</a>
         <a href=\"profile\"><i class=\"fa fa-edit\"></i> Profile</a>
         <a href=\"password\" class=\"current\"><i class=\"fa fa-key\"></i> Change password</a>
         <a href=\"close\"><i class=\"fa fa-close\"></i> Close Account</a>
         </div>";
     } else if ($n === 4) {
-        echo "<div class=\"block links-section\">
+        echo "<div class=\"block sidebar\">
         <a href=\"/settings\"><i class=\"fa fa-cog\"></i> Settings</a>
         <a href=\"profile\"><i class=\"fa fa-edit\"></i> Profile</a>
         <a href=\"password\"><i class=\"fa fa-key\"></i> Change password</a>
