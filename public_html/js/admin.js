6db62871d65d6ffc2d0b7c14c121c10a65e6e475
Sean <seanysean700@gmail.com>
2018-08-30 17:09:25 -0300

    A rough implementation of keyboard navigation for admin auto complete

--- public_html/js/admin.js
+++ public_html/js/admin.js
@@ -1,36 +1,73 @@
 const username = document.getElementById('username'),
       autocomplete = document.getElementById('autocomplete-container');
-username.addEventListener('keyup',()=>{
-    autocomplete.innerHTML = '';
-    if (username.value.length > 0) {
-        autocomplete.style.display = 'block';
-        const xhr = new XMLHttpRequest(),
-              url = `/autocomplete?limit=5&username=${username.value}`;
-        xhr.responseType = 'json';
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState === xhr.DONE) {
-                const res = xhr.response;
-                if (!res) {
-                    autocomplete.innerHTML = '<p>No user found</p>';
-                } else {
-                    res.forEach(obj=>{
-                        const el = document.createElement('div');
-                        el.classList = 'user';
-                        el.innerHTML = `<i class="fa fa-${obj.icon} state ${obj.state}"></i> ${obj.username}`;
-                        el.addEventListener('click',()=>{
-                            const usr = el.innerText.replace(' ','');
-                            username.value = usr;
-                            document.getElementById('search').submit();
-                        });
-                        autocomplete.appendChild(el);
-                    });
-                }
+let users = [],
+    current = 0,
+    last,
+    selected,
+    next;
+username.addEventListener('keyup',e=>{
+    if (e.which === 40 || e.which === 38 && users.length > 0) {
+        if (last) {
+            last.classList.remove('selected');
+        }
+        if (e.which === 40) {
+            if (current < users.length) {
+                current++;
+                next = users[current - 1];
+                next.classList.add('selected');
+                last = next;
+                selected = true;
+                console.log(current);
+            } else {
+                selected = false;
+                current = 6;
+            }
+        } else {
+            if (current > 1) {
+                current--;
+                next = users[current - 1];
+                next.classList.add('selected');
+                last = next;
+                selected = true;
+            } else {
+                selected = false;
+                current = 0;
             }
         }
-        xhr.open('GET',url);
-        xhr.send();
     } else {
-        autocomplete.style.display = 'none';
+        autocomplete.innerHTML = '';
+        if (username.value.length > 0) {
+            autocomplete.style.display = 'block';
+            const xhr = new XMLHttpRequest(),
+                  url = `/autocomplete?limit=5&username=${username.value}`;
+            xhr.responseType = 'json';
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState === xhr.DONE) {
+                    const res = xhr.response;
+                    if (!res) {
+                        autocomplete.innerHTML = '<p>No user found</p>';
+                    } else {
+                        users = [];
+                        res.forEach(obj=>{
+                            const el = document.createElement('div');
+                            el.classList = 'user';
+                            el.innerHTML = `<i class="fa fa-${obj.icon} state ${obj.state}"></i> ${obj.username}`;
+                            el.addEventListener('click',()=>{
+                                const usr = el.innerText.replace(' ','');
+                                username.value = usr;
+                                document.getElementById('search').submit();
+                            });
+                            autocomplete.appendChild(el);
+                            users.push(el);
+                        });
+                    }
+                }
+            }
+            xhr.open('GET',url);
+            xhr.send();
+        } else {
+            autocomplete.style.display = 'none';
+        }
     }
 });
 document.addEventListener('click',()=>{
@@ -38,7 +75,13 @@ document.addEventListener('click',()=>{
         autocomplete.style.display = 'none';
     }
 });
-
+document.addEventListener('keypress',e=>{
+     if (e.which === 13 && selected) {
+        e.preventDefault();
+        username.value = next.innerText.split(' ').join('');
+        selected = false;
+    }
+});
 document.querySelectorAll('.edit').forEach(el=>{
     el.addEventListener('click',()=>{
         let id = el.id.split('edit')[1];
