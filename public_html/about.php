3817df4b1ca66f827a5c3b57e10bce7ee4a7a0a1
Sean <seanysean700@gmail.com>
2018-11-29 16:36:27 -0400

    Add link to browser support wiki
    + remove rel="nofollow" on links.

--- public_html/about.php
+++ public_html/about.php
@@ -4,11 +4,13 @@ $metaDescription = 'Learn about LearnChess, a free chess website with no premium
 $pageContent = '<h1 class="block-title">About LearnChess</h1>
 <p>&nbsp;&nbsp;LearnChess is a small website for getting better at chess.
 You can <a href="puzzles/next">practice chess tactics</a>, <a href="computer">play the computer</a>, and try the <a href="coordinates">coordinates trainer</a>.
-All of these features are 100% free and there are zero ads (like <a href="https://lichess.org" rel="nofollow" target="_blank">Lichess <i class="fa fa-external-link"></i></a>).
-LearnChess is similar to <a href="https://chessvariants.training" rel="nofollow" target="_blank">Chessvariants.training <i class="fa fa-external-link"></i></a> in that anyone can contribute a puzzle.
+All of these features are 100% free and there are zero ads (like <a href="https://lichess.org" target="_blank">Lichess <i class="fa fa-external-link"></i></a>).
+LearnChess is similar to <a href="https://chessvariants.training" target="_blank">Chessvariants.training <i class="fa fa-external-link"></i></a> in that anyone can contribute a puzzle.
 </p>
 <h2>Why use LearnChess?</h2>
-<p>&nbsp;&nbsp;Use it if you want something that is <a href="https://github.com/seanysean/LearnChess" rel="nofollow" target="_blank">open source <i class="fa fa-external-link"></i></a>, 100% free (no premium accounts), and has no advertisements.
+<p>&nbsp;&nbsp;Use it if you want something that is <a href="https://github.com/seanysean/LearnChess" target="_blank">open source <i class="fa fa-external-link"></i></a>, 100% free (no premium accounts), and has no advertisements.
 Since all puzzles are submitted and reviewed by real people, they should be more instructive than computer generated puzzles.
-</p>';
+</p>
+<h2>Browser Support</h2>
+<p>&nbsp;&nbsp;Learnchess works in most modern browsers. More details <a href="https://github.com/seanysean/LearnChess/wiki/Supported-browsers" target="blank">at the learnchess wiki page</a>.';
 include "../templates/page.php";
