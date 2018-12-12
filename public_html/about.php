5ad8c6877a6e1ea159b4cb524a570afbfd0d1023
Sean <seanysean700@gmail.com>
2018-12-12 14:49:47 -0400

    Add ids to about page headings

--- public_html/about.php
+++ public_html/about.php
@@ -1,16 +1,16 @@
 <?php 
 $pageTitle = 'About';
 $metaDescription = 'Learn about LearnChess, a free chess website with no premium accounts and no ads.';
-$pageContent = '<h1 class="block-title">About LearnChess</h1>
+$pageContent = '<h1 class="block-title" id="about-learnchess">About LearnChess</h1>
 <p>&nbsp;&nbsp;LearnChess is a small website for getting better at chess.
 You can <a href="puzzles/next">practice chess tactics</a>, <a href="computer">play the computer</a>, and try the <a href="coordinates">coordinates trainer</a>.
 All of these features are 100% free and there are zero ads (like <a href="https://lichess.org" target="_blank">Lichess <i class="fa fa-external-link"></i></a>).
 LearnChess is similar to <a href="https://chessvariants.training" target="_blank">Chessvariants.training <i class="fa fa-external-link"></i></a> in that anyone can contribute a puzzle.
 </p>
-<h2>Why use LearnChess?</h2>
+<h2 id="why-use-learnchess">Why use LearnChess?</h2>
 <p>&nbsp;&nbsp;Use it if you want something that is <a href="https://github.com/seanysean/LearnChess" target="_blank">open source <i class="fa fa-external-link"></i></a>, 100% free (no premium accounts), and has no advertisements.
 Since all puzzles are submitted and reviewed by real people, they should be more instructive than computer generated puzzles.
 </p>
-<h2>Browser Support</h2>
-<p>&nbsp;&nbsp;Learnchess works in most modern browsers. More details <a href="https://github.com/seanysean/LearnChess/wiki/Supported-browsers" target="blank">at the learnchess wiki page</a>.';
+<h2 id="browser-support">Browser Support</h2>
+<p>&nbsp;&nbsp;Learnchess works in most modern browsers. More details <a href="https://github.com/seanysean/LearnChess/wiki/Supported-browsers" target="blank">at the learnchess wiki page <i class="fa fa-external-link"></i></a>.';
 include "../templates/page.php";
