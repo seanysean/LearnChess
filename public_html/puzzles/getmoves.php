d085ba59bc1551976f0087c6d4ab443855bc5423
Sean <seanysean700@gmail.com>
2020-07-27 12:35:16 -0300

    Fix puzzles not being rated
    Basically there was a strict comparision and this forces them to be integers

--- public_html/puzzles/getmoves.php
+++ public_html/puzzles/getmoves.php
@@ -9,7 +9,7 @@ if(isset($_GET['move']) && isset($_GET['movenum']) && isset($_GET['puzzle'])) {
     $puzzle_preview = isset($_GET['preview']) && $_GET['preview'] === 'true';
     $move = secure($_GET['move']);
     $movenum = secure($_GET['movenum']);
-    $puzzle = secure($_GET['puzzle']);
+    $puzzle = secure($_GET['puzzle']) + 0;
     $table = $puzzle_preview ? 'puzzles_to_review' : 'puzzles_approved';
     $sql = "SELECT fen,pgn FROM `$table` WHERE id='$puzzle'";
     $result = mysqli_query($connection,$sql);
